0083688028d1cd47fc8c681ef1df44c9dabf0ee7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-05 13:56:57 +0200

    started to css planning

--- src/app/planning/planning.module.ts
+++ src/app/planning/planning.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { PlanningRoutingModule } from './planning-routing.module';
 import { PlanningComponent } from './planning.component';
-
 
 @NgModule({
   declarations: [PlanningComponent],
   imports: [
     CommonModule,
-    PlanningRoutingModule
-  ]
+    PlanningRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
-export class PlanningModule { }
+export class PlanningModule {}
