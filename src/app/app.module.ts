d5dd19271c938d147892c91e3280ef6b44381113
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-05 11:25:24 +0200

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './routes/home/home.component';
+import { LegalComponent } from './routes/legal/legal.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, LegalComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
