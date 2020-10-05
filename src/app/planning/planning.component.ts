4174c3fbd54791e0b1940d04c4d2463a734cd3a8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-05 16:28:50 +0200

    show planning done

--- src/app/planning/planning.component.ts
+++ src/app/planning/planning.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-planning',
   templateUrl: './planning.component.html',
-  styleUrls: ['./planning.component.scss']
+  styleUrls: ['./planning.component.scss'],
 })
 export class PlanningComponent implements OnInit {
+  tasks = [
+    {
+      name: 'Installation de chantier',
+      selected: [1, 1, 0, 0, 0, 0, 0],
+    },
+    {
+      name: 'Maconnerie Gros oeuvre',
+      selected: [1, 1, 1, 0, 0, 0, 0],
+    },
+    {
+      name: 'Charpente et couverture',
+      selected: [0, 1, 1, 1, 0, 0, 0],
+    },
+    {
+      name: 'Menuiserie',
+      selected: [0, 1, 1, 1, 1, 1, 0],
+    },
+    {
+      name: 'Menuiserie',
+      selected: [0, 1, 1, 1, 1, 1, 0],
+    },
+    {
+      name: 'Menuiserie',
+      selected: [0, 1, 1, 1, 1, 1, 0],
+    },
+    {
+      name: 'Menuiserie',
+      selected: [0, 1, 1, 1, 1, 1, 0],
+    },
+    {
+      name: 'Menuiserie',
+      selected: [0, 1, 1, 1, 1, 1, 0],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
