e89c005644af46ad7f6446abee3b365dec48010a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-05 19:02:10 +0200

    fullscreen ios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,15 @@ import { LegalComponent } from './routes/legal/legal.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'legal', component: LegalComponent },
-  { path: 'planning', loadChildren: () => import('./planning/planning.module').then(m => m.PlanningModule) },
+  {
+    path: 'planning',
+    loadChildren: () =>
+      import('./planning/planning.module').then((m) => m.PlanningModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
